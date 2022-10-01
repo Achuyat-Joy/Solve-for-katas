b1be54ad57fe22d02412ccae83241f931649ed03
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2022-10-02 02:34:24 +0600

    one kata solution added

--- js/main.js
+++ js/main.js
@@ -15,3 +15,19 @@ function solution(str){
 }
 
 console.log(solution('word'))
+
+
+// 2. number in descending order
+
+function descendingOrder(n){
+
+  n = n
+  .toString()
+  .split('')
+  .sort((a,b)=>b-a)
+  .join('');
+
+  return parseInt(n);
+}
+
+console.log(descendingOrder(0))
