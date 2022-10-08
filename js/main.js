05cf175622afc2ea97dda06d3074d7b61f161dc3
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2022-10-09 03:28:06 +0600

    9

--- js/main.js
+++ js/main.js
@@ -124,3 +124,15 @@ function removeChar(str){
   return str;
 
 };
+
+
+// 9. Remove String Spaces
+
+function noSpace(x){
+    
+  return x.replace(/\s+/g,'');
+}
+
+
+console.log(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'));
+
