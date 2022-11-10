dff6918fff0bff59bbe17dae452368378b4857d5
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2022-11-11 00:38:18 +0600

    42

--- js/main.js
+++ js/main.js
@@ -613,3 +613,19 @@ function otherAngle(a, b) {
 }
 
 console.log(otherAngle(30, 60));
+
+// 42.Calculate BMI
+
+function bmi(weight, height) {
+  let bmi = weight / Math.pow(height, 2);
+
+  return bmi <= 18.5
+    ? "Underweight"
+    : bmi <= 25.0
+    ? "Normal"
+    : bmi <= 30.0
+    ? "Overweight"
+    : "Obese";
+}
+
+console.log(bmi(80, 1.8));
