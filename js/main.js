e47f28b0cdb531c0f1bce4028915498dea21fda5
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2023-01-15 01:24:31 +0600

    97

--- js/main.js
+++ js/main.js
@@ -1458,3 +1458,22 @@ console.log(
     "London",
   ])
 );
+
+// 97. Age Range Compatibility Equation
+
+function datingRange(age) {
+  let min = 0;
+  let max = 0;
+
+  if (age <= 14) {
+    min = Math.floor(age - 0.1 * age);
+    max = Math.floor(age + 0.1 * age);
+  } else {
+    min = Math.floor(age / 2 + 7);
+    max = Math.floor((age - 7) * 2);
+  }
+
+  return `${min}-${max}`;
+}
+
+console.log(datingRange(17));
