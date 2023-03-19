8417fa566d392b7e52f840a1c629381b88ec5234
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2023-03-19 12:03:51 +0600

    143

--- js/main.js
+++ js/main.js
@@ -2025,3 +2025,17 @@ function smash(words) {
 }
 
 console.log(smash(["hello", "world"]));
+
+// 143. Powers of 2
+
+function powersOfTwo(n) {
+  let a = [];
+
+  for (let i = 0; i <= n; i++) {
+    a.push(Math.pow(2, i));
+  }
+
+  return a;
+}
+
+console.log(powersOfTwo(4));
