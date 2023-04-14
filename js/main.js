05c4db8f6db4fa87588ce4ed50354c2210e94c0d
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2023-04-14 13:04:56 +0600

    164

--- js/main.js
+++ js/main.js
@@ -2269,3 +2269,9 @@ function between(a, b) {
 function sayHello(name) {
   return `Hello, ${name}`;
 }
+
+// 164. Training JS #7: if..else and ternary operator
+
+function saleHotdogs(n) {
+  return n < 5 ? n * 100 : n >= 5 && n < 10 ? n * 95 : n * 90;
+}
