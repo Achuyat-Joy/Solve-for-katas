9815e514111c4345f50254a1a4e67cdd3b4a42e1
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2023-05-07 12:46:48 +0600

    173

--- js/main.js
+++ js/main.js
@@ -2360,3 +2360,11 @@ function stringy(size) {
 }
 
 console.log(stringy(1));
+
+// 173. Training JS #1: create your first JS function and print "Hello World!"
+
+function helloWorld() {
+  var str = "Hello World!";
+
+  console.log(str);
+}
