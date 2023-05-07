070875597f47d8626f00a7373600bf43943815e4
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2023-05-08 02:42:35 +0600

    174

--- js/main.js
+++ js/main.js
@@ -2368,3 +2368,9 @@ function helloWorld() {
 
   console.log(str);
 }
+
+// 174. What's the real floor? (in america ground floor is added and no 13th floor)
+
+function getRealFloor(n) {
+  return n <= 0 ? n : n > 0 && n < 13 ? n - 1 : n - 2;
+}
