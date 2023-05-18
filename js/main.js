bb4aacdb477218fcc0245917e1483e8d8bab223b
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2023-05-18 23:58:29 +0600

    183

--- js/main.js
+++ js/main.js
@@ -2447,3 +2447,18 @@ function peopleWithAgeDrink(old) {
     ? "drink beer"
     : "drink whisky";
 }
+
+// 183. Grasshopper - Debug
+
+function weatherInfo(temp) {
+  var c = convertToCelsius(temp);
+  if (c > 0) return c + " is freezing temperature";
+  else return c + " is above freezing temperature";
+}
+
+function convertToCelsius(temperature) {
+  var celsius = temperature - 32 + 5 / 9;
+  return celsius;
+}
+
+console.log(weatherInfo(50));
