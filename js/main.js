3b828d587e4d829a994cacbaab59f3f3c35305ec
Achuyat Saha Joy <47113699+Achuyat-Joy@users.noreply.github.com>
2023-06-10 11:51:20 +0600

    194

--- js/main.js
+++ js/main.js
@@ -2636,3 +2636,13 @@ function popElement(arr) {
 
   return arr;
 }
+
+// 194. Fix your code before the garden dies!
+
+function rainAmount(mm) {
+  if (mm < 40) {
+    return "You need to give your plant " + (40 - mm) + "mm of water";
+  } else {
+    return "Your plant has had more than enough water for today!";
+  }
+}
